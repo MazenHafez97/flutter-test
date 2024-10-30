b2f512809db2c74910e4776747de992a5846c78a
mazen <mazenhafez18@gmail.com>
2024-10-30 10:59:49 +0300

    editing

---
+++ lib/detail.dart
@@ -0,0 +1,228 @@
+import 'package:flutter/material.dart';
+import 'package:untitled5/homepage.dart';
+
+class itemsdateils extends StatefulWidget {
+  final data;
+  const itemsdateils({super.key, this.data});
+
+  @override
+  State<itemsdateils> createState() => _itemsdateilsState();
+}
+
+class _itemsdateilsState extends State<itemsdateils> {
+  int selected = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          iconSize: 40,
+          selectedItemColor: Colors.orange,
+          onTap: (val) {
+            setState(() {
+              selected = val;
+            });
+          },
+          currentIndex: selected,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "*"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_bag_outlined), label: "*"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_2_outlined), label: "*"),
+          ]),
+      endDrawer: Drawer(
+        backgroundColor: Colors.black,
+        child: Container(
+          padding: EdgeInsets.all(15),
+          child: ListView(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 60,
+                    height: 60,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(60),
+                      child:
+                          Image.asset("images/mazen2.jpg", fit: BoxFit.cover),
+                    ),
+                  ),
+                ],
+              ),
+              const Row(
+                children: [
+                  Expanded(
+                      child: ListTile(
+                    title: Text(
+                      "Mazen Hafez",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      "mazen.hafez18@gmail.com",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w200,
+                          fontSize: 13),
+                    ),
+                  )),
+                ],
+              ),
+              ListTile(
+                title: const Text(
+                  "home",
+                  style: TextStyle(color: Colors.white),
+                ),
+                leading: Icon(Icons.home),
+                iconColor: Colors.white,
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => Homepage()));
+                },
+              ),
+              ListTile(
+                title: const Text("Account"),
+                textColor: Colors.white,
+                leading: const Icon(Icons.account_balance_rounded),
+                iconColor: Colors.white,
+                onTap: () {},
+              ),
+              ListTile(
+                title: const Text("order"),
+                textColor: Colors.white,
+                leading: const Icon(Icons.check_box),
+                iconColor: Colors.white,
+                onTap: () {},
+              ),
+              ListTile(
+                title: const Text("about Us"),
+                textColor: Colors.white,
+                leading: const Icon(Icons.help),
+                iconColor: Colors.white,
+                onTap: () {},
+              ),
+              ListTile(
+                title: const Text("contact"),
+                textColor: Colors.white,
+                leading: const Icon(Icons.phone),
+                iconColor: Colors.white,
+                onTap: () {},
+              ),
+              ListTile(
+                title: const Text("setting"),
+                textColor: Colors.white,
+                leading: const Icon(Icons.settings),
+                iconColor: Colors.white,
+                onTap: () {},
+              ),
+              ListTile(
+                title: const Text("Sign Out"),
+                textColor: Colors.white,
+                leading: const Icon(Icons.exit_to_app),
+                iconColor: Colors.white,
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+      appBar: AppBar(
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.store_mall_directory_outlined,
+              color: Colors.black,
+            ),
+            Text(
+              " Mazen ",
+              style: TextStyle(color: Colors.black),
+            ),
+            Text(
+              "Story",
+              style: TextStyle(color: Colors.orange),
+            )
+          ],
+        ),
+        iconTheme: const IconThemeData(color: Colors.grey),
+      ),
+      body: ListView(
+        children: [
+          Image.asset(widget.data["images"]),
+          Text(
+            widget.data["title"],
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              widget.data["subtitile"],
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.grey),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 15, bottom: 25),
+            child: Text(
+              widget.data["price"],
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  color: Colors.orange, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Color : ", style: TextStyle(color: Colors.grey)),
+              const SizedBox(
+                width: 10,
+              ),
+              Container(
+                height: 20,
+                width: 20,
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(color: Colors.orange)),
+              ),
+              const Text("  Gray  "),
+              const SizedBox(
+                width: 10,
+              ),
+              Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.black,
+                ),
+              ),
+              const Text("  Black  ")
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 20),
+            child: const Text(
+              "Size :  39  40  41  42  43",
+              style: TextStyle(color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 110, vertical: 20),
+            child: MaterialButton(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              color: Colors.black,
+              onPressed: () {},
+              child: const Text(
+                "+Add To Cart",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled5/detail.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -8,11 +9,66 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  List categories = [
+    {"iconname": Icons.laptop, "title": "laptob"},
+    {"iconname": Icons.phone_iphone, "title": "Phone"},
+    {"iconname": Icons.watch_outlined, "title": "Watch"},
+    {"iconname": Icons.headphones_outlined, "title": "Headphone"},
+  ];
+  List items = [
+    {
+      "images": "images/air pods.png",
+      "title": "Air Pods Pro",
+      "subtitile": "wosem Air Pod",
+      "price": "500EL"
+    },
+    {
+      "images": "images/head.png",
+      "title": "Headphone",
+      "subtitile": "Good Headphone",
+      "price": "350EL"
+    },
+    {
+      "images": "images/la.png",
+      "title": "Laptpob",
+      "subtitile": "The Best Laptob",
+      "price": "8000EL"
+    },
+    {
+      "images": "images/watch2.png",
+      "title": "Appel Watch",
+      "subtitile": "smart watch",
+      "price": "750EL"
+    },
+    {
+      "images": "images/wctch.png",
+      "title": "Appel Watch",
+      "subtitile": "smart watch",
+      "price": "850EL"
+    },
+  ];
+  int selected = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          iconSize: 40,
+          selectedItemColor: Colors.orange,
+          onTap: (val) {
+            setState(() {
+              selected = val;
+            });
+          },
+          currentIndex: selected,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "*"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_bag_outlined), label: "*"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_2_outlined), label: "*"),
+          ]),
       body: Container(
-        padding: EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
         child: ListView(
           children: [
             Row(
@@ -20,14 +76,14 @@ class _HomepageState extends State<Homepage> {
                 Expanded(
                     child: TextFormField(
                   decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.search),
+                      prefixIcon: const Icon(Icons.search),
                       prefixIconColor: Colors.black,
                       hintText: "search",
                       border: InputBorder.none,
                       fillColor: Colors.grey[200],
                       filled: true),
                 )),
-                Padding(
+                const Padding(
                   padding: EdgeInsets.only(left: 20),
                   child: Icon(
                     Icons.menu,
@@ -39,17 +95,90 @@ class _HomepageState extends State<Homepage> {
             Container(
               height: 30,
             ),
-            Text(
+            const Text(
               "Categories",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
             ),
-            Row(
-              children: [
-                Column(
-                  children: [Container()],
-                )
-              ],
-            )
+            Container(
+              height: 100,
+              child: ListView.builder(
+                itemCount: categories.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, i) {
+                  return Container(
+                    margin: EdgeInsets.only(right: 22),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(50)),
+                          child: Icon(categories[i]["iconname"], size: 40),
+                          padding: EdgeInsets.all(15),
+                        ),
+                        Container(
+                            child: Text(
+                          categories[i]["title"],
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Colors.grey[800]),
+                        )),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: const Text(
+                "Best Selling",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
+            ),
+            GridView.builder(
+                itemCount: items.length,
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, mainAxisExtent: 205),
+                itemBuilder: (context, i) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => itemsdateils(
+                                data: items[i],
+                              )));
+                    },
+                    child: Card(
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            width: 200,
+                            color: Colors.grey[400],
+                            child: Image.asset(
+                              items[i]["images"],
+                              fit: BoxFit.fill,
+                              height: 111,
+                            ),
+                          ),
+                          Text(
+                            items[i]["title"],
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          Text(items[i]["subtitile"]),
+                          Text(
+                            items[i]["price"],
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                })
           ],
         ),
       ),
